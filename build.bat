290d853b91f491bfba1ec8e228260c5fb424e7d6
Feodor Fitsner <feodor@appveyor.com>
2021-11-07 14:43:31 -0800

    Do not build installer

--- build.bat
+++ build.bat
@@ -27,27 +27,8 @@ mkdir "%DIST%"
 REM Create the "no install" zip version
 for %%a in ("%BIN%") do (buildtools\zip -j -9 -r "%DIST%\nvm-noinstall.zip" "%CD%\LICENSE" %%a\* -x "%BIN%\nodejs.ico")
 
-REM Generate update utility
-cd .\updater
-go build nvm-update.go
-move nvm-update.exe %DIST%
-cd ..\
-
-REM Generate the installer (InnoSetup)
-buildtools\iscc "%INNOSETUP%" "/o%DIST%"
-buildtools\zip -j -9 -r "%DIST%\nvm-setup.zip" "%DIST%\nvm-setup.exe"
-buildtools\zip -j -9 -r "%DIST%\nvm-update.zip" "%DIST%\nvm-update.exe"
-
-del %DIST%\nvm-update.exe
-del %DIST%\nvm-setup.exe
-
 REM Generate checksums
 for %%f in (%DIST%\*.*) do (certutil -hashfile "%%f" MD5 | find /i /v "md5" | find /i /v "certutil" >> "%%f.checksum.txt")
-
-REM Cleanup
-del %BIN%\nvm.exe
-del %BIN%\nvm-update.exe
-del %BIN%\nvm-setup.exe
 
 echo NVM for Windows v%AppVersion% build completed.
 @echo on
